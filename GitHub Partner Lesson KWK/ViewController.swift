837b4aff73f8ed146effeea24ba162903628b55e
Abira <abiraskaur@gmai.com>
2022-06-28 16:02:22 -0500

    updates

--- GitHub Partner Lesson KWK/ViewController.swift
+++ GitHub Partner Lesson KWK/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //Do any additional setup after loading the view.
+        
+        
+       /*myLayer.backgroundColor = UIColor.blue.cgColor
+        self.view.layer.addSublayer(myLayer)
+        let myLayer: CALayer = .init()*/
+        
     }
+    
+    /*@IBAction func transitionfrompage1(_ sender: Any) {
+        var mFrame = CGRect(x: 0, y: view.frame.size.height, width: view.frame.size.width, height: 0)
+        myLayer.frame = mFrame
+        let fillingAnim = CABasicAnimation(keyPath: "bounds")
+        fillingAnim.fillMode = CAMediaTimingFillMode.forwards
+        fillingAnim.fromValue = mFrame
+        fillingAnim.duration = 2
+        fillingAnim.isRemovedOnCompletion = false
+        mFrame.size.height = 1000
+        fillingAnim.toValue = mFrame
+        myLayer.add(fillingAnim, forKey: nil)
+    }*/
+    
 
-
+    
 }
 
